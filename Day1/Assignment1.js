f891c99aec98e4f04c9e85aa2645d510e54a9adb
Rohan Mishra <mishrarohan090@gmail.com>
2023-10-02 22:08:20 +0545

    Few Changes

--- Day1/Assignment1.js
+++ Day1/Assignment1.js
@@ -3,11 +3,16 @@ let context = canvas.getContext("2d");
 
 //Background
 context.beginPath();
+context.strokeStyle = "Gray";
+context.lineWidth = 3;
 context.rect(122,180,260,70);
 context.fill();
+context.stroke();
 
 // red light
 context.beginPath();
+context.strokeStyle = "white";
+context.lineWidth = 3;
 context.fillStyle = "Red";
 context.arc(180,215,25,0,360);
 context.fill();
